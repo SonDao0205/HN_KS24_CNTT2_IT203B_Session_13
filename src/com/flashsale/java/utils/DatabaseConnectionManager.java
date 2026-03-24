6c996682488369530a10ff7f85ca4c441ccdb911
AnHaiDung <anhaidung2k6@gmail.com>
2026-03-24 15:46:56 +0700

    up

--- src/com/flashsale/java/utils/DatabaseConnectionManager.java
+++ src/com/flashsale/java/utils/DatabaseConnectionManager.java
@@ -9,7 +9,7 @@ public class DatabaseConnectionManager {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/flash_sale?createDatabaseIfNotExist=true";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "123";
+    private static final String PASSWORD = "06072006";
 
     public static Connection openConnection(){
         try {
